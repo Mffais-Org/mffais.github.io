82a5ead5724cb44f3ce57e07b8b731c15c4c23e4
Josip-Muzic <josip.muzic@cinnamon.agency>
2021-02-26 09:36:12 +0100

    Add horizontal scroll on phone for notifications section

--- src/sections/notifications/index.js
+++ src/sections/notifications/index.js
@@ -4,31 +4,40 @@ import style from './Notifications.module.scss';
 
 export default function () {
   return (
-    <section className={ style.notifications }>
-      <div className="grid-three">
-        <div className={ `${ style.phone } col` }>
-          <img src="/img/notifications-1.svg" />
-        </div>
-        <div className={ `${ style.phone } col` }>
-          <img src="/img/notifications-2.svg" />
-        </div>
-        <div className="col-text-with-image">
-          <div>
-            <div className="float-right">
-              <div className={ style.smiley }>
-                <img src="/img/smiley.svg" />
+    <div className={ style.borderBottom }>
+      <section className={ style.notifications }>
+        <div className="grid-three">
+          <div className={ `${ style.phoneImgForDesktop } col` }>
+            <img src="/img/notifications-1.svg" />
+          </div>
+          <div className={ `${ style.phoneImgForDesktop } col` }>
+            <img src="/img/notifications-2.svg" />
+          </div>
+
+          <div className="col-text-with-image">
+            <div>
+              <div className="float-right">
+                <div className={ style.smiley }>
+                  <img src="/img/smiley.svg" />
+                </div>
+                <h3>
+                  Due date <br className={ style.phoneBr } />
+                  notifications <br />
+                  help <br className={ style.phoneBr } />
+                  you stay <br />
+                  on top of <br className={ style.phoneBr } />
+                  your <br />
+                  cash flow
+                </h3>
               </div>
-              <h3>
-                Due date <br className={ style.phoneBr }/>
-                notifications <br />
-                help <br className={ style.phoneBr }/>you stay <br />
-                on top of <br className={ style.phoneBr }/>your <br />
-                cash flow
-              </h3>
             </div>
           </div>
         </div>
+      </section>
+      <div className={ `${ style.phoneImgForMobile } col` }>
+        <img src="/img/notifications-1.svg" />
+        <img src="/img/notifications-2.svg" />
       </div>
-    </section>
+    </div>
   );
 }
