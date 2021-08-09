ae469105a9eaf0b5942201c97252987de4de2b4f
Marino Kolarić <marino.kolaric@cinnamon.agency>
2021-08-09 11:31:52 +0200

    Feat: add contact details

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -10,16 +10,30 @@ export default function () {
         <h4>Give it a try!</h4>
         <GooglePlay />
         <h4>Have any questions?</h4>
-        <a className={ `button ${ style.contactSupport }` } target="_blank" href="mailto: support@mffais.com">Contact support</a>
+        <a
+          className={ `button ${ style.contactSupport }` }
+          target="_blank"
+          href="mailto: support@mffais.com"
+        >
+          Contact support
+        </a>
       </div>
       <footer>
-        <Link to="/#">
-          <img src="/img/logo.svg" />
-        </Link>
-        <Link to="/#features">Features</Link>
-        <Link to="/#moneyFlow">Money flow</Link>
-        <Link to="/privacy-policy/#">Privacy Policy</Link>
-        <Link to="/terms-and-conditions/#">Terms and conditions</Link>
+        <div className={ style.nav }>
+          <Link to="/#">
+            <img src="/img/logo.svg" />
+          </Link>
+          <Link to="/#features">Features</Link>
+          <Link to="/#moneyFlow">Money flow</Link>
+          <Link to="/privacy-policy/#">Privacy Policy</Link>
+          <Link to="/terms-and-conditions/#">Terms and conditions</Link>
+        </div>
+        <div className={ style.contactInfo }>
+          <p>2436 E 4th Street, Ste #336</p>
+          <p>Long Beach, CA 90814</p>
+          <p><a href="mailto:support@mffais.com">support@mffais.com</a></p>
+          <p>844-463-3247 / 844-4-MFFAIS (USA)</p>
+        </div>
       </footer>
     </>
   );
