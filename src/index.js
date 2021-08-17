e22e86166c26eb87c671f55e74db90a753b594f6
MarinoKolaric <marinokolaric@gmail.com>
2021-08-17 11:54:34 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import { createBrowserHistory } from "history";
+import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
 
--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -2,38 +2,39 @@ import React, { useState, useEffect } from 'react';
 import style from '../components/privacy-and-terms/PrivacyAndTerms.module.scss';
 
 export default function () {
-  const [ privacyPolicy, setPrivacyPolicy ] = useState([]);
+  // const [ privacyPolicy, setPrivacyPolicy ] = useState([]);
 
-  const getData = (url) => {
-    fetch(`/data/${ url }.json`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    }).then(async (response) => {
-      setPrivacyPolicy(await response.json());
-    });
-  };
+  // const getData = (url) => {
+  //   fetch(`/data/${ url }.json`, {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Accept: 'application/json',
+  //     },
+  //   }).then(async (response) => {
+  //     setPrivacyPolicy(await response.json());
+  //   });
+  // };
 
-    useEffect(() => {
-      getData('privacy-policy', setPrivacyPolicy);
-    }, []);
+  //   useEffect(() => {
+  //     getData('privacy-policy', setPrivacyPolicy);
+  //   }, []);
+  return 'hey test'
 
-  return (
-    <>
-      <section className={ style.header }>
-        <h2>Mffais Privacy Policy TEST2</h2>
-      </section>
-      <section className={ style.content }>
-        {privacyPolicy.map((group, groupIndex) => (
-          <div key={ `group${ groupIndex }` }>
-            <p className={ style.title }>{group.title}</p>
-            {group.text.map((text, index) => (
-              <p key={ `text${ groupIndex }-${ index }` }>{text}</p>
-            ))}
-          </div>
-        ))}
-      </section>
-    </>
-  );
+  // return (
+  //   <>
+  //     <section className={ style.header }>
+  //       <h2>Mffais Privacy Policy TEST2</h2>
+  //     </section>
+  //     <section className={ style.content }>
+  //       {privacyPolicy.map((group, groupIndex) => (
+  //         <div key={ `group${ groupIndex }` }>
+  //           <p className={ style.title }>{group.title}</p>
+  //           {group.text.map((text, index) => (
+  //             <p key={ `text${ groupIndex }-${ index }` }>{text}</p>
+  //           ))}
+  //         </div>
+  //       ))}
+  //     </section>
+  //   </>
+  // );
 }
