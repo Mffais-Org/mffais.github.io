ad032bd76b60e9dfbd2107eda2981cad67801417
Bruno Kozina <bruno.kozina@cinnamon.agency>
2024-08-29 15:27:58 +0200

    Add language swticher

--- src/components/language-switcher/LanguageSwitcher.tsx
+++ src/components/language-switcher/LanguageSwitcher.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import i18n from "../../i18n";
 import style from "./LanguageSwitcher.module.scss";
@@ -9,13 +9,120 @@ const LanguageSwitcher = () => {
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
   };
-
+  const [open, setOpen] = useState(false);
+  console.log(open);
   return (
-    <div className={style.container}>
-      <button onClick={() => changeLanguage("en")}>
-        {t("button.english")}
-      </button>
-      <button onClick={() => changeLanguage("fr")}>{t("button.french")}</button>
+    <div className={style.container} onClick={() => setOpen(!open)}>
+      <div className={style.dropdownTitle}>Select language</div>
+      {open && (
+        <div className={style.dropdownContainer}>
+          <div className={style.language} onClick={() => changeLanguage("en")}>
+            English
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("fr")}>
+            French
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("bg")}>
+            Bulgarian
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("bn")}>
+            Bengali
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("ca")}>
+            Catalan
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("cs")}>
+            Czech
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("da")}>
+            Danish
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("de")}>
+            German
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("el")}>
+            Greek
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("es")}>
+            Spanish
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("fi")}>
+            Finnish
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("hi")}>
+            Hindi
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("hu")}>
+            Hungarian
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("id")}>
+            Indonesian
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("it")}>
+            Italian
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("ja")}>
+            Japanese
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("ko")}>
+            Korean
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("lt")}>
+            Lithuanian
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("mr")}>
+            Marathi
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("ms")}>
+            Malay
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("nl")}>
+            Dutch
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("no")}>
+            Norwegian
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("pl")}>
+            Polish
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("pt")}>
+            Portuguese
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("ro")}>
+            Romanian
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("ru")}>
+            Russian
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("sk")}>
+            Slovak
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("sr")}>
+            Serbian
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("sv")}>
+            Swedish
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("th")}>
+            Thai
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("tl")}>
+            Tagalog
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("tr")}>
+            Turkish
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("uk")}>
+            Ukrainian
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("vi")}>
+            Vietnamese
+          </div>
+          <div className={style.language} onClick={() => changeLanguage("zh")}>
+            Chinese
+          </div>
+        </div>
+      )}
     </div>
   );
 };
