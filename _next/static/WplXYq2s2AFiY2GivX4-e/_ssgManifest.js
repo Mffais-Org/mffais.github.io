177446965c1195f0e3f2b5ea2b801b7ead35cff0
entwwwicklung <luka.siljak@gmail.com>
2025-02-19 20:25:53 +0100

    Updates

--- _next/static/WplXYq2s2AFiY2GivX4-e/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fapp-guide","\u002F[locale]\u002Flegal\u002Fprivacy-policy","\u002F[locale]\u002Flegal\u002Fterms-and-conditions"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
