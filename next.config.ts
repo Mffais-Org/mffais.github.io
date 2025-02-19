03eee648c38141d7ae385bfa25903f023ec21745
Zoran Latković <105056049+Concept42@users.noreply.github.com>
2025-02-19 18:06:47 +0100

    static out

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  distDir: 'build',
+  distDir: "build",
+  output: "export",
   reactStrictMode: true,
+  trailingSlash: true,
 };
 
 export default withNextIntl(nextConfig);
