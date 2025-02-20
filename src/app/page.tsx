81fedd88e70d890656d5c8377c455cee53c38733
ZoranLatkovic1 <zoran.latkovic@cinnamon.agency>
2025-02-20 08:24:46 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,7 @@
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 const Page = () => {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const locale = window.navigator.language;
-      redirect(`/${locale}`);
-    }
-  }, []);
-
-  return null;
+  return redirect("/en");
 };
 
 export default Page;
