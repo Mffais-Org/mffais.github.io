a3ed1745077f0309c800a8c4b908ad67e07ab139
entwwwicklung <luka.siljak@gmail.com>
2025-02-20 12:57:26 +0100

    Updates

---
+++ _next/static/chunks/app/layout-33a604da04dbeac8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1254:(a,e,l)=>{Promise.resolve().then(l.t.bind(l,429,23))},429:a=>{a.exports={style:{fontFamily:"'Rubik', 'Rubik Fallback'",fontStyle:"normal"},className:"__className_b539cb",variable:"__variable_b539cb"}}},a=>{var e=e=>a(a.s=e);a.O(0,[816,441,517,358],()=>e(1254)),_N_E=a.O()}]);
