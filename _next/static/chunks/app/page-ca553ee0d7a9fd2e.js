94ec6c2b5eea33d19099228bad11eee5f7af045c
Kevin <kevin@mffais.com>
2025-07-02 10:26:08 -0600

    fixed js files

--- _next/static/chunks/app/page-ca553ee0d7a9fd2e.js
+++ _next/static/chunks/app/page-ca553ee0d7a9fd2e.js
@@ -1,1 +1,27 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{7052:(e,r,s)=>{Promise.resolve().then(s.bind(s,9809))},6046:(e,r,s)=>{"use strict";s.r(r);var t=s(6658),a={};for(let e in t)"default"!==e&&(a[e]=()=>t[e]);s.d(r,a)},9809:(e,r,s)=>{"use strict";s.r(r),s.d(r,{default:()=>a});var t=s(6046);let a=()=>(0,t.redirect)("/en")}},e=>{var r=r=>e(e.s=r);e.O(0,[441,517,358],()=>r(7052)),_N_E=e.O()}]);
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [974], {
+        7052: (e, r, s) => {
+            Promise.resolve().then(s.bind(s, 9809))
+        },
+        6046: (e, r, s) => {
+            "use strict";
+            s.r(r);
+            var t = s(6658),
+                a = {};
+            for (let e in t) "default" !== e && (a[e] = () => t[e]);
+            s.d(r, a)
+        },
+        9809: (e, r, s) => {
+            "use strict";
+            s.r(r), s.d(r, {
+                default: () => a
+            });
+            var t = s(6046);
+            let a = () => (0, t.redirect)("/en")
+        }
+    },
+    e => {
+        var r = r => e(e.s = r);
+        e.O(0, [441, 517, 358], () => r(7052)), _N_E = e.O()
+    }
+]);
